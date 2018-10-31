1f3854c6cf443e0e8d60ec28d8eaf34a05a43c18
Igor Petrovski <igor@home.local>
2018-10-31 17:51:31 -0400

    Config key map

--- keymap_k104.c
+++ keymap_k104.c
@@ -3,69 +3,52 @@
 //https://github.com/kekstee/tmk_keyboard/tree/master/keyboard/nerd
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap 0: Default Layer
-     * ,---------------------------------------------------------------.
-     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Psc|Pus|Del|
-     * |---------------------------------------------------------------|
-     * |`  |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Hom|
-     * |---------------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|PgU|
-     * |---------------------------------------------------------------|
-     * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |PgD|
-     * |---------------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Up |End|
-     * |---------------------------------------------------------------|
-     * |Ctrl|Win |Alt |          Space         |Alt|Fn |Ctr|Lef|Dow|Rig|
-     * `---------------------------------------------------------------'
+     * ,----------------------------------------------------------------------------------------------------.
+     * |Esc|     F1| F2| F3| F4|   F5| F6| F7| F8|    F9|F10|F11|F12|  Pscr|Slck|Paus|                      |
+     * |----------------------------------------------------------------------------------------------------|
+     * |`  |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp  |  Ins |Hom |PgU |  |Lnum|Psls|Past|Pmns|
+     * |----------------------------------------------------------------------------------------------------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \ |  Del |End |PgD |  |P7  |P8  |P9  |Ppls|
+     * |-----------------------------------------------------------------------------------------------|    |
+     * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return   |                   |P4  |P5  |P6  |    |
+     * |----------------------------------------------------------------------------------------------------|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift      |      |Up  |       |P1  |P2  |P3  |Pent|
+     * |-----------------------------------------------------------------------------------------------|    |
+     * |Ctrl|Win |Alt |          Space         |Alt|Win |  Fn| Ctrl |  Lef |Dow |Rig |  |P0       |Pdot|    |
+     * `----------------------------------------------------------------------------------------------------'
      */
+
     KEYMAP(
-        ESC,F1,  F2, F3,   F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,PSCR,PAUS, DEL, \
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSPC,     HOME, \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,     BSLS,PGUP, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,       ENT,PGDN, \
-        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,        RSFT, UP, END, \
-        LCTL,LGUI,LALT,            SPC,                RALT, FN0,RCTL,LEFT,DOWN,RGHT),
+        ESC,F1,  F2, F3,   F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,     PSCR,SLCK,PAUS, \
+        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSPC, INS, HOM, PGU, LNUM,PSLS,PAST,PMNS, \
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,      DEL, END, PGD,   P7,  P8,  P9,PPLS,\
+        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,  ENT,                  P4,  P5,  P6,\
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,        RSFT, UP, END,        P1,  P2,  P3,PENT,\
+        LCTL,LGUI,LALT,            SPC,                RALT, FN0,RCTL,LEFT,DOWN,RGHT         P0,       PDOT),
     /* Keymap 1: FN Layer 2
-     * ,---------------------------------------------------------------.
-     * |   |   |   |   |   |   |   |Rev|Pau| FF|Mut|VoD|VoU|   |   |   |
-     * |---------------------------------------------------------------|
-     * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |   |
-     * |---------------------------------------------------------------|
-     * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |   |
-     * |---------------------------------------------------------------|
-     * |      |   |   |   |   |   |   |   |   |   |   |   |        |   |
-     * |---------------------------------------------------------------|
-     * |        |   |   |   |   |   |   |   |   |   |   |      |   |   |
-     * |---------------------------------------------------------------|
-     * |    |    |    |                        |   |   |   |   |   |   |
-     * `---------------------------------------------------------------'
+     * ,----------------------------------------------------------------------------------------------------.
+     * |Esc|     F1| F2| F3| F4|   F5|Rev|Pau |Mut | VoD|VoU|F11|F12|  Pscr|Slck|Paus|                      |
+     * |----------------------------------------------------------------------------------------------------|
+     * |`  |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp  |  Ins |Hom |PgU |  |Lnum|Psls|Past|Pmns|
+     * |----------------------------------------------------------------------------------------------------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \ |  Del |End |PgD |  |P7  |P8  |P9  |Ppls|
+     * |-----------------------------------------------------------------------------------------------|    |
+     * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return   |                   |P4  |P5  |P6  |    |
+     * |----------------------------------------------------------------------------------------------------|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift      |      |Up  |       |P1  |P2  |P3  |Pent|
+     * |-----------------------------------------------------------------------------------------------|    |
+     * |Ctrl|Win |Alt |          Space         |Alt|Win |  Fn| Ctrl |  Lef |Dow |Rig |  |P0       |Pdot|    |
+     * `----------------------------------------------------------------------------------------------------'
      */
+   
     KEYMAP(
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MRWD,MPLY,MFFD,MUTE,VOLD,VOLU,TRNS,TRNS, INS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS, TRNS,      TRNS,TRNS, \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS,TRNS, TRNS,      TRNS,TRNS,TRNS, \
-        TRNS,TRNS,TRNS,           TRNS,                TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+        TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MRWD,MPLY,MFFD,MUTE,VOLD,VOLU, TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS, TRNS, TRNS,                    TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS,TRNS, TRNS,      TRNS,       TRNS,        TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,            TRNS,                  TRNS,TRNS,TRNS,TRNS,  TRNS,TRNS,TRNS,   TRNS,     TRNS),
 };
 const action_t PROGMEM fn_actions[] = {
     [0]  = ACTION_LAYER_MOMENTARY(1),
 };
-
-
-
-
-    /* Keymap 0: Default Layer
-     * ,----------------------------------------------------------------------------------------------.
-     * |Esc|     F1| F2| F3| F4|  F5| F6| F7| F8|     F9|F10|F11|F12|  Psc|SLK|Pus|                   |
-     * |----------------------------------------------------------------------------------------------|
-     * |`  |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp  |  INS|Hom|PgU|    NLK|NU/|NU*|NU-|
-     * |----------------------------------------------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \ |  Del|End|PgD|    NU7|NU8|NU9|NU+|
-     * |------------------------------------------------------------------------------------------|   |
-     * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return   |                  NU4|NU5|NU6|   |
-     * |----------------------------------------------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift      |      Up |        NU1|NU2|NU3|ENT|
-     * |------------------------------------------------------------------------------------------|   |
-     * |Ctrl|Win |Alt |          Space         |Alt|Win |  Fn| Ctrl |  Lef|Dow|Rig|    NU0    |Del|   |
-     * `----------------------------------------------------------------------------------------------'
-     */
