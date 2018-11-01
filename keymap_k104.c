c4222393fe7d72d37b787e4dfe16b642d84a73a5
Igor Petrovski <igor@home.local>
2018-10-31 20:19:29 -0400

    fixed keymap and matrix pinout type

--- keymap_k104.c
+++ keymap_k104.c
@@ -19,12 +19,12 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     KEYMAP(
-        ESC,F1,  F2, F3,   F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,     PSCR,SLCK,PAUS, \
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSPC, INS, HOM, PGU, LNUM,PSLS,PAST,PMNS, \
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,      DEL, END, PGD,   P7,  P8,  P9,PPLS,\
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,  ENT,                  P4,  P5,  P6,\
-        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,        RSFT, UP, END,        P1,  P2,  P3,PENT,\
-        LCTL,LGUI,LALT,            SPC,                RALT, FN0,RCTL,LEFT,DOWN,RGHT         P0,       PDOT),
+         ESC,       F1,  F2, F3,   F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, PSCR,SLCK,PAUS, \
+         GRV,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSPC, INS,HOME,PGUP,     LNUM,PSLS,PAST,PMNS, \
+         TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,BSLS, DEL, END,PGDN,     P7,  P8,  P9,PPLS,\
+        CAPS,   A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,  ENT,                    P4,  P5,  P6,\
+        LSFT,   Z,   X,   C,   V,   B,   N,   M,    COMM,DOT, SLSH,      RSFT,        UP,         P1,  P2,  P3,PENT,\
+        LCTL,LGUI,LALT,             SPC,                  RALT,LGUI, FN0,RCTL,  LEFT,DOWN,RGHT,   P0,       PDOT),
     /* Keymap 1: FN Layer 2
      * ,---------------------------------------------------------------------------------=-------------------.
      * |Esc|   | F1| F2| F3| F4|  | F5|Rev|Pau|Mut|  |VoD|VoU|F11|F12| |Pscr|Slck|Paus|                      |
